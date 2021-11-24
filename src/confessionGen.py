d89b4316f218029506038e9806432c6b25022c11
Comfort-Twala <kontreitroos@gmail.com>
2021-11-24 21:38:53 +0200

    confessionGen method addConfession added

--- src/confessionGen.py
+++ src/confessionGen.py
@@ -24,4 +24,26 @@ class ConfessionGenerator:
 	addConfession method to write confession onto the background image
 	"""
 	def addConfession(self, confession, overlay, bg_img):
-		pass
+		draw = ImageDraw.Draw(overlay)
+		font = ImageFont.truetype(self.FONT, 55)
+		width, height = bg_img.size
+		# Checking if there's a confesser
+		if '-' in confession and confession[-3:] != '...':
+			## writing the confesser's name at the bottom
+			confesser = confession[confession.rfind('-'):]
+			confesser = textwrap.wrap(confesser, width=30)
+			text = confession[:confession.rfind('-')]
+			padding = 865
+			for line in confesser:
+				w, h = font.getsize(line)
+				draw.text(((width - w) / 2, padding), line, font=font, fill="white")
+				padding += self.pad
+		else:
+			text = confession
+		# Wrapping the confession into a paragraph		
+		para = textwrap.wrap(text, width=55)
+		## Writing the confession
+		for line in para:
+			w, h = font.getsize(line)
+			draw.text(((width - w) / 2, ((height - 180 - h) / 2) - (40 * len(para))), line, font=font, fill="white")
+			height += h + self.pad
