dd03c740f26234c3bfba1c934b8db3590978d375
Comfort-Twala <kontreitroos@gmail.com>
2021-11-24 23:15:47 +0200

    Main class methods complete

--- src/Main.py
+++ src/Main.py
@@ -1,19 +1,44 @@
+import confessionGen
+
 """
 Main class that provides user interface and entrance to program
 """
 class Main:
+	def __init__(self):
+		self.file = "input/confessions.txt"
+		self.encoding = "utf-8"
+
 	"""
 	Program usage menu
 	"""
-	def usage():
-		pass
+	def usage(self):
+		usage = """Usage:\nTo use program ensure that all your 
+confessions are written in the confessions.txt
+file in the input folder."""
+		return usage
 
 	"""
 	Program Driver
 	"""
-	def main():
+	def main(self):
 		print("\tCONFESSIONS GENERATOR")
 		print("=====================================")
+		print("    ***  Starting operation  ***")
+		try:
+			with open(self.file, encoding=self.encoding) as file:
+				confessions = file.readlines()
+		except FileNotFoundError:
+			print("Error: Oops ... couldn't find that file\n")
+			print(self.usage)
+			confessions = []
+
+		generator = confessionGen.ConfessionGenerator(confessions=confessions)
+		generator.generate()
+
+		print("DONE\n*** Check the output folder!")
+		print("=====================================")
+		print("\t@trof_talks_tech")
+
 
 if __name__ == "__main__":
-	Main.main()
+	Main().main()
