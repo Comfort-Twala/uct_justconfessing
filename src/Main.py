aa0333157ee0ac815a66a6b2751bf105562cfa85
Comfort-Twala <kontreitroos@gmail.com>
2021-11-24 22:08:54 +0200

    Main class skeleton added

---
+++ src/Main.py
@@ -0,0 +1,19 @@
+"""
+Main class that provides user interface and entrance to program
+"""
+class Main:
+	"""
+	Program usage menu
+	"""
+	def usage():
+		pass
+
+	"""
+	Program Driver
+	"""
+	def main():
+		print("\tCONFESSIONS GENERATOR")
+		print("=====================================")
+
+if __name__ == "__main__":
+	Main.main()
